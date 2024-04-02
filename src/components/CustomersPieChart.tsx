0aebba20f5c6308d923cd4aa0ecbc5565d6e6edb
AkhilPanwar <panwarakhil1811@gmail.com>
2024-04-02 15:58:18 +0530

    functionality-final-commit

--- src/components/CustomersPieChart.tsx
+++ src/components/CustomersPieChart.tsx
@@ -1,9 +1,20 @@
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 
 import { PieChart, Pie, Cell, Legend } from "recharts";
 import Customers from "../data/customer.json";
 
 export default function CustomersPieChart() {
+  const [totalNew, setTotalNew] = useState(0);
+  useEffect(() => {
+    let newTotal = 0;
+    Customers.customerData.forEach((val) => {
+      if (val.type === "new") {
+        newTotal += val.value;
+      }
+    });
+    setTotalNew(newTotal);
+  }, []);
   return (
     <Box
       display="flex"
@@ -32,23 +43,50 @@ export default function CustomersPieChart() {
         </Box>
       </Box>
       <PieChart width={270} height={270}>
+        <text
+          x={135}
+          y={120}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={30}
+          fontWeight="bold"
+        >
+          {`${totalNew}%`}
+        </text>
+        <text
+          x={130}
+          y={144}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={16}
+          fontWeight={400}
+          fill="#222222"
+        >
+          Total New
+        </text>
+        <text
+          x={132}
+          y={160}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={17}
+          fontWeight={400}
+          fill="#222222"
+        >
+          Customers
+        </text>
         <Pie
           data={Customers.customerData}
           dataKey="value"
           innerRadius={70}
-          outerRadius={100}
-          startAngle={Customers.customerData[0].startAngle}
-          endAngle={
-            Customers.customerData[Customers.customerData.length - 1].endAngle
-          }
+          outerRadius={85}
+          startAngle={90}
+          endAngle={460}
         >
           {Customers.customerData.map((entry, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={entry.fill}
-              strokeWidth={entry.strokeWidth}
-            />
+            <Cell key={`cell-${index + 2}`} fill={entry.fill} />
           ))}
+
           <Legend verticalAlign="bottom" height={36} />
         </Pie>
       </PieChart>
